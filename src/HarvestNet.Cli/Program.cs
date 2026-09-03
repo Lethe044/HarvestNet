8dfb142ff17a2f959cde84d883730d53714c51e8
Shade_Lethe <kivanc.yilmaz044@gmail.com>
2026-09-03 21:53:46 +0300

    Fix build: restore missing AngleSharp using directive in CLI Program.cs

--- src/HarvestNet.Cli/Program.cs
+++ src/HarvestNet.Cli/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using AngleSharp;
 using HarvestNet.Browser;
 using HarvestNet.Core;
 using HarvestNet.Core.Crawling;
@@ -310,7 +311,7 @@ public static class Program
 
     private static IEnumerable<Uri> ExtractLinks(string html, Uri baseUrl, string linkSelector)
     {
-        var context = AngleSharp.BrowsingContext.New(AngleSharp.Configuration.Default);
+        var context = BrowsingContext.New(Configuration.Default);
         var document = context.OpenAsync(req => req.Content(html).Address(baseUrl.AbsoluteUri)).GetAwaiter().GetResult();
 
         var links = new List<Uri>();
